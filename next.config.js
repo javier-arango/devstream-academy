aec609f00f4e96ac614cd65f67af04cbeb0e89d0
Javier Arango <javierarango.developer@gmail.com>
2023-12-04 16:37:51 -0500

    initial setup

--- next.config.js
+++ next.config.js
@@ -1,21 +1,20 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'img.youtube.com',
-          pathname: '/vi/**',
-        },
-        {
-          protocol: 'https',
-          hostname: 'i.ytimg.com',
-          pathname: '/vi/**',
-        },
-      ],
-    },
-  }
-  
-  module.exports = nextConfig
-  
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'img.youtube.com',
+        pathname: '/vi/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.ytimg.com',
+        pathname: '/vi/**',
+      },
+    ],
+  },
+}
+
+module.exports = nextConfig
